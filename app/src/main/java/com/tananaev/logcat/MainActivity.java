20cbc7237321772533912372c18379de1c952d33
Anton Tananaev <anton.tananaev@gmail.com>
2016-06-24 18:42:40 +1200

    Add recycler view to the main screen

--- app/src/main/java/com/tananaev/logcat/MainActivity.java
+++ app/src/main/java/com/tananaev/logcat/MainActivity.java
@@ -2,6 +2,8 @@ package com.tananaev.logcat;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RecyclerView recyclerView = (RecyclerView) findViewById(android.R.id.list);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(new LineAdapter());
     }
+
 }

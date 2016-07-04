6b74f095067bb198ad3560eb0d8ad7cf0c7a7d74
Anton Tananaev <anton.tananaev@gmail.com>
2016-07-04 14:24:34 +1200

    Implement log file sharing

--- app/src/main/java/com/tananaev/logcat/MainActivity.java
+++ app/src/main/java/com/tananaev/logcat/MainActivity.java
@@ -1,6 +1,23 @@
+/*
+ * Copyright 2016 Anton Tananaev (anton.tananaev@gmail.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.tananaev.logcat;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
@@ -21,6 +38,9 @@ import com.tananaev.adblib.AdbCrypto;
 import com.tananaev.adblib.AdbStream;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
@@ -40,7 +60,9 @@ public class MainActivity extends AppCompatActivity {
     private static final String KEY_PUBLIC = "publicKey";
     private static final String KEY_PRIVATE = "privateKey";
 
-    public static final long ANIMATION_DURATION = 75;
+    private static final String TEMP_FILE = "/logcat.txt";
+
+    private static final long ANIMATION_DURATION = 75;
 
     private RecyclerView recyclerView;
     private LineAdapter adapter;
@@ -133,6 +155,31 @@ public class MainActivity extends AppCompatActivity {
         readerTask.execute();
     }
 
+    private Intent getShareIntent() {
+        File file = new File(getExternalCacheDir() + TEMP_FILE);
+        if (file.exists()) {
+            file.delete();
+        }
+        try  {
+            file.createNewFile();
+            BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+            for (Line line : adapter.getLines()) {
+                writer.write(line.getContent());
+                writer.newLine();
+            }
+            writer.close();
+        } catch (IOException e) {
+            Log.w(TAG, e);
+        }
+
+        Uri uri = Uri.fromFile(new File(getExternalCacheDir() + TEMP_FILE));
+
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_STREAM, uri);
+        return intent;
+    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
@@ -163,6 +210,7 @@ public class MainActivity extends AppCompatActivity {
             updateScrollState(!scroll);
             return true;
         } else if (item.getItemId() == R.id.action_share) {
+            startActivity(Intent.createChooser(getShareIntent(), getString(R.string.menu_share)));
             return true;
         }
         return false;
